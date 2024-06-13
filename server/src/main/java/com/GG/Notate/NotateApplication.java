bc92a2543cb96f29179fb0f47f1482ed9003ed06
“GowthamG30” <gowtham.gorrepati30@gmail.com>
2024-06-13 12:42:59 +0530

    Add base Spring Boot application setup

---
+++ server/src/main/java/com/GG/Notate/NotateApplication.java
@@ -0,0 +1,13 @@
+package com.GG.Notate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotateApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/GG/Notate/NotateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GG.Notate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
